dd1fdcd01a58c340f3b93bb7e6010fd190b19724
Yifan Yang <yifanyeung@sjtu.edu.cn>
2025-05-28 23:27:36 +0800

    update

--- pipeline/audio_download/download_from_json.sh
+++ pipeline/audio_download/download_from_json.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 JSON_FILE=$1
-archive_file="download_$(basename "$JSON_FILE" .json).txt"
+WORKSPACE_ROOT=$2
+channel_name=$3
+archive_file=${WORKSPACE_ROOT}/"download_${channel_name}.txt"
+output_dir=${WORKSPACE_ROOT}/${channel_name}
+
+mkdir -p $output_dir
 
 while IFS= read -r line; do
   video_id=$(echo "$line" | jq -r .id)
@@ -10,6 +15,6 @@ while IFS= read -r line; do
 
   yt-dlp -f 'ba' \
     --download-archive $archive_file \
-    --cookies cookies.txt \
-    "$youtube_url" -o '%(channel_id)s#%(id)s.%(ext)s'
+    --cookies ${WORKSPACE_ROOT}/cookies.txt \
+    "$youtube_url" -o ${output_dir}/'%(channel_id)s#%(id)s.%(ext)s'
 done < $JSON_FILE
